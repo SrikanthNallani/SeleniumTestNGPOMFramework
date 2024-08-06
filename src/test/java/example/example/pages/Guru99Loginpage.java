be0bce56d93762dae11c6beadeea9b218a2c3bcc
nikhi <nikhi@Nikhila.attlocal.net>
2024-08-05 23:12:22 -0400

    Added Guru99 related classes

---
+++ src/test/java/example/example/pages/Guru99Loginpage.java
@@ -0,0 +1,30 @@
+package example.example.pages;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Guru99Loginpage extends BasePage{
+	public Guru99Loginpage(WebDriver driver) {
+		super(driver);
+		
+	}
+	@FindBy(name = "uid")
+	private WebElement username;
+	@FindBy(name= "password")
+	private WebElement password;
+	public void username(String id) {
+		username.sendKeys(id);
+		
+		
+	}
+	public void password(String pass) {
+		password.sendKeys(pass);
+		
+		
+	}
+	
+
+}
+
---
+++ src/test/java/example/example/tests/Guru99Logintest.java
@@ -0,0 +1,32 @@
+package example.example.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import example.example.factory.PageinstancesFactory;
+import example.example.pages.FacebookLoginPage;
+import example.example.pages.Guru99Loginpage;
+
+/**
+ * The Class Guru99Logintest.
+ *
+ * @author Nikhila
+ */
+@Test(testName = "Guru99 login test", description = "Guru99 login test")
+public class Guru99Logintest extends BaseTest {
+
+	@Test
+	public void Guru99Logintest() {
+		driver.get("https://demo.guru99.com/v4/");
+		Guru99Loginpage guru99loginpage =  PageinstancesFactory.getInstance(Guru99Loginpage.class);
+		guru99loginpage.username("Nikhila");
+		guru99loginpage.password("abcd");
+		
+		/*
+		 * FacebookLoginPage facebookLoginPage =
+		 * PageinstancesFactory.getInstance(FacebookLoginPage.class);
+		 * facebookLoginPage.enterEmail("abc").enterPassword("abc").clickSignIn();
+		 * Assert.assertFalse(false, "Login Sucess");
+		 */
+	}
+}
