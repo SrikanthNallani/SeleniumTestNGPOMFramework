aae5f06ce079d4c34fa1795ccdbb5ae3a1969db0
nikhi <nikhi@Nikhila.attlocal.net>
2024-08-07 22:24:55 -0400

    Test

--- src/test/java/example/example/tests/Guru99registrationTest.java
+++ src/test/java/example/example/tests/Guru99registrationTest.java
@@ -24,7 +24,7 @@ public class Guru99registrationTest extends BaseTest {
 		guru99loginpage.username("mngr583941");
 		guru99loginpage.password("zUsUzEr");
 		guru99loginpage.clicklogin();
-		/* Registration steps*/
+		/* Registration steps v*/
 		Guru99registrationpage registration = PageinstancesFactory.getInstance(Guru99registrationpage.class);
 		registration.clickNewcust();
 		/*
